47db6d48f1926d1c04cb87b0feb76fd80f8d2fb9
Aleksei99 <smurahaleha@gmail.com>
2020-04-18 17:30:21 +0300

    added Embeddable from Halina branch

--- database/src/main/java/by/work_company/entity/Contact.java
+++ database/src/main/java/by/work_company/entity/Contact.java
@@ -16,18 +16,25 @@ import javax.persistence.*;
 public class Contact {
 
     @OneToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column(name = "telephone", unique = true)
+    @Column(name = "telephone", unique = true, nullable = false)
     private String telephone;
 
-    @Column(name = "address")
-    private String address;
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "home_city")),
+            @AttributeOverride(name = "street", column = @Column(name = "home_street")),
+            @AttributeOverride(name = "house", column = @Column(name = "home_house")),
+            @AttributeOverride(name = "number", column = @Column(name = "home_number"))
+    })
+    private Address homeAddress;
 
-    @Column(name = "other_address")
-    private String otherAddress;
-
-    @Column(name = "email", unique = true)
-    private String email;
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "other_city")),
+            @AttributeOverride(name = "street", column = @Column(name = "other_street")),
+            @AttributeOverride(name = "house", column = @Column(name = "other_house")),
+            @AttributeOverride(name = "number", column = @Column(name = "other_number"))
+    })
+    private Address otherAddress;
 }
