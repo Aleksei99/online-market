2befa9bc3b4161643c831fb0106d6a97c68794e9
Aleksei99 <smurahaleha@gmail.com>
2020-08-02 19:02:55 +0300

    edited inputs added remove product in cart

--- web/src/main/java/by/work/web/controller/CartController.java
+++ web/src/main/java/by/work/web/controller/CartController.java
@@ -4,13 +4,13 @@ import by.work.database.entity.Order;
 import by.work.database.entity.Product;
 import by.work.service.OrderService;
 import by.work.service.dto.OrderDTO;
-import com.sun.org.apache.xpath.internal.operations.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import java.util.Set;
@@ -26,30 +26,39 @@ public class CartController {
     }
 
     @ModelAttribute("modelOrder")
-    public OrderDTO modelOrder(){
+    public OrderDTO modelOrder() {
         return new OrderDTO();
     }
 
     @PostMapping("/ordering")
-    public String post(OrderDTO dto, HttpSession session){
-        double totalAmount=0;
-        for(int i = 0;i<dto.getCount().size();i++){
-            for(int j=0;j<dto.getPrice().size();j++){
-                if(i==j){
-                    totalAmount+=dto.getCount().get(i)*dto.getPrice().get(j);
+    public String post(OrderDTO dto, HttpSession session) {
+        double totalAmount = 0;
+        for (int i = 0; i < dto.getCount().size(); i++) {
+            for (int j = 0; j < dto.getPrice().size(); j++) {
+                if (i == j) {
+                    totalAmount += dto.getCount().get(i) * dto.getPrice().get(j);
                 }
             }
         }
         Order order = (Order) session.getAttribute("currentOrder");
-        orderService.update(order,totalAmount);
+        orderService.update(order, totalAmount);
         return "redirect:/ordering";
     }
 
+    @PostMapping("/product/remove")
+    public String removeProduct(HttpSession session, @RequestParam(value = "id", required = false) Long id) {
+        Order order = (Order) session.getAttribute("currentOrder");
+        order.getProducts().removeIf(product -> product.getId().equals(id));
+        session.removeAttribute("currentOrder");
+        session.setAttribute("currentOrder", order);
+        return "redirect:/cart";
+    }
+
     @GetMapping("/cart")
-    public String getPage(HttpSession session, Model model){
+    public String getPage(HttpSession session, Model model) {
         Order order = (Order) session.getAttribute("currentOrder");
         Set<Product> products = order.getProducts();
-        model.addAttribute("cartProducts",products);
+        model.addAttribute("cartProducts", products);
         return "cart";
     }
 
